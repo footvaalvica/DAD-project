9f596222ef007c79184c480d87b25882fe51b9c7
Mateus Pinho <mateusleitepinho@gmail.com>
2023-10-24 08:33:55 +0100

    put back code that was wrongly removed

--- TKVTransactionManager/Services/ServerService.cs
+++ TKVTransactionManager/Services/ServerService.cs
@@ -217,8 +217,10 @@ namespace TKVTransactionManager.Services
                 tasks.Add(t);
             }
 
-            // they should all be the same, so we can just wait for one
-            Task.WaitAny(tasks.ToArray());
+            for (var i = 0; i < _leaseManagers.Count / 2 + 1; i++)
+            {
+                tasks.RemoveAt(Task.WaitAny(tasks.ToArray()));
+            }
 
             Monitor.Enter(this);
 
